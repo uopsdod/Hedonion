263f94d705b30a7f65ce3c013182f5290499d91c
SamTsai <uopsdod@gmail.com>
2016-06-05 21:47:38 +0800

    firstCommit
    next:
    upload subpages

---
+++ default.php
@@ -0,0 +1,63 @@
+<?
+
+?>
+<html>
+  
+<head>
+  <title> Hedonion </title>  
+  <meta name="description" content="Religion: Hedonism">
+  <meta name="keywords" content="Hedonism,Hedonion,happiness,joy">
+  <meta name="author" content="Sam Tsai">
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="index.css">
+</head>
+
+<div class="all">  
+ <header> Hedonion </header>
+ <!-- separating line -->
+ <body>
+   <a href="http://codepen.io/webwebwebwebDesign/full/eispC" id="mainpage">main page</a>
+   <br/><br/><br/><br/>
+   <div class="list1">
+   <a href="http://codepen.io/webwebwebwebDesign/full/tEziq"> About Hedonion </a>
+   <a href="http://codepen.io/webwebwebwebDesign/full/LcBij"> Why do we live? </a>
+   <a href="http://codepen.io/webwebwebwebDesign/full/jqbnB"> How to accumulate happiness?</a><a  href="http://codepen.io/webwebwebwebDesign/full/rlKBD"> Find your true happiness here </a> 
+   
+   </div>
+ 
+   <!-- <img class="symbo1" src="http://goo.gl/UE6yyj" alt="Symbol"/>   - 圖片抓不到-->
+   
+   <div class="textblock">
+   <p>
+  <p>
+  Have you ever feel confused about your future?
+  </p>
+  <p>Have you ever wondered why you were born at the very beginning? 
+  </p>
+  <p>
+  Have you ever question the meaning of your life?
+  </p>
+  <p>
+    Then, here is where you can find all the answers. Hedonion.
+  </p>
+   </div>
+  <br/><br/><br/><br/><br/><br/><br/>
+  <hr/>
+  <footer> 
+    <p>Contact information:<br/>
+      e-mail: officialhedonion@gmail.com
+    </p> 
+  </footer> 
+ </body>
+</div>
+
+
+
+
+
+
+
+
+
+
+</html>
